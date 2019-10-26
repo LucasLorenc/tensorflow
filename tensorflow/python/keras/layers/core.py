555b592a38f8234f41d7e0964b037c17744ad588
Lucas Lorenc <lukaslorenc.sk@gmail.com>
2019-10-26 11:44:12 +0200

    adding DropConnectDense layer

--- tensorflow/python/keras/layers/core.py
+++ tensorflow/python/keras/layers/core.py
@@ -1151,3 +1151,185 @@ class ActivityRegularization(Layer):
     config = {'l1': self.l1, 'l2': self.l2}
     base_config = super(ActivityRegularization, self).get_config()
     return dict(list(base_config.items()) + list(config.items()))
+
+
+@keras_export('keras.layers.DropConnectDense')
+class DropConnectDense(Layer):
+  """Just your regular densely-connected NN layer.
+
+  `Dense` implements the operation:
+  `output = activation(dot(input, kernel) + bias)`
+  where `activation` is the element-wise activation function
+  passed as the `activation` argument, `kernel` is a weights matrix
+  created by the layer, and `bias` is a bias vector created by the layer
+  (only applicable if `use_bias` is `True`).
+
+  Note: If the input to the layer has a rank greater than 2, then
+  it is flattened prior to the initial dot product with `kernel`.
+
+  Example:
+
+  ```python
+  # as first layer in a sequential model:
+  model = Sequential()
+  model.add(Dense(32, input_shape=(16,)))
+  # now the model will take as input arrays of shape (*, 16)
+  # and output arrays of shape (*, 32)
+
+  # after the first layer, you don't need to specify
+  # the size of the input anymore:
+  model.add(Dense(32))
+  ```
+
+  Arguments:
+    units: Positive integer, dimensionality of the output space.
+    activation: Activation function to use.
+      If you don't specify anything, no activation is applied
+      (ie. "linear" activation: `a(x) = x`).
+    use_bias: Boolean, whether the layer uses a bias vector.
+    kernel_initializer: Initializer for the `kernel` weights matrix.
+    bias_initializer: Initializer for the bias vector.
+    kernel_regularizer: Regularizer function applied to
+      the `kernel` weights matrix.
+    bias_regularizer: Regularizer function applied to the bias vector.
+    activity_regularizer: Regularizer function applied to
+      the output of the layer (its "activation")..
+    kernel_constraint: Constraint function applied to
+      the `kernel` weights matrix.
+    bias_constraint: Constraint function applied to the bias vector.
+    kernel_dropout: Float between 0 and 1.
+      Fraction of the weight units to drop.
+
+  Input shape:
+    N-D tensor with shape: `(batch_size, ..., input_dim)`.
+    The most common situation would be
+    a 2D input with shape `(batch_size, input_dim)`.
+
+  Output shape:
+    N-D tensor with shape: `(batch_size, ..., units)`.
+    For instance, for a 2D input with shape `(batch_size, input_dim)`,
+    the output would have shape `(batch_size, units)`.
+  """
+
+  def __init__(self,
+               units,
+               activation=None,
+               use_bias=True,
+               kernel_initializer='glorot_uniform',
+               bias_initializer='zeros',
+               kernel_regularizer=None,
+               bias_regularizer=None,
+               activity_regularizer=None,
+               kernel_constraint=None,
+               bias_constraint=None,
+               kernel_dropout=0.,
+               **kwargs):
+    if 'input_shape' not in kwargs and 'input_dim' in kwargs:
+      kwargs['input_shape'] = (kwargs.pop('input_dim'),)
+
+    super(DropConnectDense, self).__init__(
+        activity_regularizer=regularizers.get(activity_regularizer), **kwargs)
+
+    self.units = int(units) if not isinstance(units, int) else units
+    self.activation = activations.get(activation)
+    self.use_bias = use_bias
+    self.kernel_initializer = initializers.get(kernel_initializer)
+    self.bias_initializer = initializers.get(bias_initializer)
+    self.kernel_regularizer = regularizers.get(kernel_regularizer)
+    self.bias_regularizer = regularizers.get(bias_regularizer)
+    self.kernel_constraint = constraints.get(kernel_constraint)
+    self.bias_constraint = constraints.get(bias_constraint)
+    self.kernel_dropout = min(1., max(0., kernel_dropout))
+
+    self.supports_masking = True
+    self.input_spec = InputSpec(min_ndim=2)
+
+  def build(self, input_shape):
+    dtype = dtypes.as_dtype(self.dtype or K.floatx())
+    if not (dtype.is_floating or dtype.is_complex):
+      raise TypeError('Unable to build `Dense` layer with non-floating point '
+                      'dtype %s' % (dtype,))
+    input_shape = tensor_shape.TensorShape(input_shape)
+    if tensor_shape.dimension_value(input_shape[-1]) is None:
+      raise ValueError('The last dimension of the inputs to `Dense` '
+                       'should be defined. Found `None`.')
+    last_dim = tensor_shape.dimension_value(input_shape[-1])
+    self.input_spec = InputSpec(min_ndim=2,
+                                axes={-1: last_dim})
+    self.kernel = self.add_weight(
+        'kernel',
+        shape=[last_dim, self.units],
+        initializer=self.kernel_initializer,
+        regularizer=self.kernel_regularizer,
+        constraint=self.kernel_constraint,
+        dtype=self.dtype,
+        trainable=True)
+    if self.use_bias:
+      self.bias = self.add_weight(
+          'bias',
+          shape=[self.units,],
+          initializer=self.bias_initializer,
+          regularizer=self.bias_regularizer,
+          constraint=self.bias_constraint,
+          dtype=self.dtype,
+          trainable=True)
+    else:
+      self.bias = None
+    self.built = True
+
+  def call(self, inputs, training=None):
+      if training is None:
+          training = K.learning_phase()
+
+      ones = array_ops.ones_like(self.kernel)
+      def dropped_weight_connections():
+          return K.dropout(ones, self.kernel_dropout) * self.kernel_dropout
+      kern_dp_mask = K.in_train_phase(dropped_weight_connections, ones, training=training)
+
+      rank = len(inputs.shape)
+      if rank > 2:
+          # Broadcasting is required for the inputs.
+          outputs = standard_ops.tensordot(inputs, self.kernel * kern_dp_mask, [[rank - 1], [0]])
+          # Reshape the output back to the original ndim of the input.
+          if not context.executing_eagerly():
+              shape = inputs.shape.as_list()
+              output_shape = shape[:-1] + [self.units]
+              outputs.set_shape(output_shape)
+      else:
+          inputs = math_ops.cast(inputs, self._compute_dtype)
+          if K.is_sparse(inputs):
+              outputs = sparse_ops.sparse_tensor_dense_matmul(inputs, self.kernel * kern_dp_mask)
+          else:
+              outputs = gen_math_ops.mat_mul(inputs, self.kernel * kern_dp_mask)
+      if self.use_bias:
+          outputs = nn.bias_add(outputs, self.bias)
+      if self.activation is not None:
+          return self.activation(outputs)  # pylint: disable=not-callable
+      return outputs
+
+  def compute_output_shape(self, input_shape):
+    input_shape = tensor_shape.TensorShape(input_shape)
+    input_shape = input_shape.with_rank_at_least(2)
+    if tensor_shape.dimension_value(input_shape[-1]) is None:
+      raise ValueError(
+          'The innermost dimension of input_shape must be defined, but saw: %s'
+          % input_shape)
+    return input_shape[:-1].concatenate(self.units)
+
+  def get_config(self):
+    config = {
+        'units': self.units,
+        'activation': activations.serialize(self.activation),
+        'use_bias': self.use_bias,
+        'kernel_initializer': initializers.serialize(self.kernel_initializer),
+        'bias_initializer': initializers.serialize(self.bias_initializer),
+        'kernel_regularizer': regularizers.serialize(self.kernel_regularizer),
+        'bias_regularizer': regularizers.serialize(self.bias_regularizer),
+        'activity_regularizer':
+            regularizers.serialize(self.activity_regularizer),
+        'kernel_constraint': constraints.serialize(self.kernel_constraint),
+        'bias_constraint': constraints.serialize(self.bias_constraint),
+        'kernel_dropout': self.kernel_dropout
+    }
+    base_config = super(DropConnectDense, self).get_config()
+    return dict(list(base_config.items()) + list(config.items()))
